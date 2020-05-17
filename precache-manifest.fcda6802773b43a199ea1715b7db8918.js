a6d9a4691144c116b31988ef865bc9b056aaca8c
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-05-17 14:51:54 +0400

    Updates

---
+++ precache-manifest.fcda6802773b43a199ea1715b7db8918.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00934489d55c3265671e35407f06445",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "b599b00192248247247a",
+    "url": "/my-project/static/css/main.dba35cee.chunk.css"
+  },
+  {
+    "revision": "1f25a52e9ecbe1edec1c",
+    "url": "/my-project/static/js/2.784b833a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-project/static/js/2.784b833a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b599b00192248247247a",
+    "url": "/my-project/static/js/main.632a4111.chunk.js"
+  },
+  {
+    "revision": "daad0d67b6e51c10578b",
+    "url": "/my-project/static/js/runtime-main.dfb332f4.js"
+  },
+  {
+    "revision": "7f4f12d49bcbaa692898c0d04d32c050",
+    "url": "/my-project/static/media/editIcon.7f4f12d4.svg"
+  },
+  {
+    "revision": "c260a9eebe927da03f20ce4fcc9b5da9",
+    "url": "/my-project/static/media/loader.c260a9ee.svg"
+  },
+  {
+    "revision": "ea9db1539f16868b27d8e315c2a2f7f2",
+    "url": "/my-project/static/media/saveIcon.ea9db153.svg"
+  },
+  {
+    "revision": "49c8e403cd1929e9e7b02126824ff831",
+    "url": "/my-project/static/media/user.49c8e403.jpg"
+  }
+]);
